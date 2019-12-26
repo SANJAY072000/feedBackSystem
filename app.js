399e162aadc7e7d89086cbfff239ec777afe973d
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-12-26 16:59:54 +0530

    FeedBack System started

---
+++ app.js
@@ -0,0 +1,79 @@
+// importing the required modules
+const express=require("express"),
+passport=require("passport"),
+bodyparser=require("body-parser"),
+mongoose=require('mongoose'),
+path=require("path"),
+cors=require("cors");
+
+
+// creating the server
+const app=express();
+
+
+// importing the database schemas
+
+
+
+// importing the routes
+
+
+
+// importing the database connection string
+const dbstr=require('./setup/config').mongoUrl;
+
+
+// connecting the database
+mongoose.connect(dbstr,{useNewUrlParser:true,useUnifiedTopology:true})
+        .then(()=>console.log('Mongodb connected successfully'))
+        .catch(err=>console.log(err));
+
+
+// configuring middleware for bodyparser
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
+
+// configuring middleware for cors
+app.use(cors());
+
+
+// configuring middleware for passport
+app.use(passport.initialize());
+
+
+// declaring the port
+const port=process.env.PORT || 3000;
+
+
+// configuring the routes
+
+
+
+// deployment related stuff
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
+
+// starting the server
+app.listen(port,()=>console.log(`Server is running at port ${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ setup/config.js
@@ -0,0 +1,4 @@
+module.exports={
+    mongoUrl:'mongodb+srv://sanjay_20:san07jay@cluster0-yxg19.mongodb.net/test?retryWrites=true&w=majority',
+    secret:'Kick your ass'
+};
